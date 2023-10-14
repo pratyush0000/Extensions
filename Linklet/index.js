19d82ef15e9f5707d74d1e278f184ab6e9083f35
Pratyush Kamal <pratyushkamal88@gmail.com>
2023-10-15 00:04:19 +0530

    AdBlocker init

--- Linklet/index.js
+++ Linklet/index.js
@@ -4,7 +4,9 @@ const inputButton = document.getElementById("inputButton")
 const inputText = document.getElementById("inputText")
 const tabButton = document.getElementById("tabButton")
 const deleteButton = document.getElementById("deleteButton")
+let alertmessage = document.getElementById("alertmessage")
 let ulEl = document.getElementById("ulEl")
+
 
 
 
@@ -24,16 +26,27 @@ inputButton.addEventListener("click", function(){
     //     alert("Not a link.");
     //     return;
     // }
-    
-    myLeads.push(inputText.value)
 
-    inputText.value=""
-    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    if(inputText.value.startsWith("https://")){
+        alertmessage.textContent=""
+        myLeads.push(inputText.value)
 
-    printList(myLeads)
+        inputText.value=""
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+
+        printList(myLeads)
 
 
-    console.log(localStorage.getItem("myLeads"))
+        console.log(localStorage.getItem("myLeads"))
+    }
+    else if(inputText.value=""){
+        alertmessage.textContent="Empty input field. Paste a valid link."
+        inputText.value=""
+    }
+    else{
+        alertmessage.textContent="Not a link. Please paste a valid link."
+        inputText.value=""
+    }
 })
 
 
@@ -42,10 +55,12 @@ inputButton.addEventListener("click", function(){
 tabButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (tabs && tabs[0]) {
+            alertmessage.textContent=""
             myLeads.push(tabs[0].url);
             localStorage.setItem("myLeads", JSON.stringify(myLeads));
             printList(myLeads);
         } else {
+            alertmessage.textContent="No actice tab found"
             console.log("No active tab found.");
         }
     });
