2c0ead5886cf284ced1b9ee96b18cc3994e6b3e2
Pratyush Kamal <pratyushkamal88@gmail.com>
2023-10-17 01:29:14 +0530

    old ver restore

---
+++ Linklet 1.0.0/index.js
@@ -0,0 +1,87 @@
+//inputbutton
+let myLeads = []
+const inputButton = document.getElementById("inputButton")
+const inputText = document.getElementById("inputText")
+const tabButton = document.getElementById("tabButton")
+const deleteButton = document.getElementById("deleteButton")
+let alertmessage = document.getElementById("alertmessage")
+let ulEl = document.getElementById("ulEl")
+
+
+
+
+const ls = JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(ls)
+
+if(ls){
+    myLeads=ls
+    printList(myLeads)
+}
+
+
+inputButton.addEventListener("click", function(){
+
+    // if (!url.startsWith("https://")) {
+    //     alert("Not a link.");
+    //     return;
+    // }
+    myLeads.push(inputText.value)
+
+    inputText.value=""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+
+    printList(myLeads)
+
+
+    console.log(localStorage.getItem("myLeads"))
+})
+
+
+
+
+tabButton.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (tabs && tabs[0]) {
+            alertmessage.textContent=""
+            myLeads.push(tabs[0].url);
+            localStorage.setItem("myLeads", JSON.stringify(myLeads));
+            printList(myLeads);
+        } else {
+            alertmessage.textContent="No actice tab found"
+            console.log("No active tab found.");
+        }
+    });
+});
+
+// tabButton.addEventListener("click", function(){
+
+//     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+//         myLeads.push(tabs[0].URL)
+//         localStorage.setItem("myLeads",JSON.stringify(myLeads))
+//         printList(myLeads)
+//     })
+// })
+
+
+deleteButton.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads=[]
+    printList(myLeads)
+    // ulEl.textContent=myLeads
+})
+
+function printList(e){
+    let listItems = ""
+    for(let i=0;i<e.length;i++){
+        listItems += `
+            <li>
+                <a href = '${e[i]}' target='_blank'>
+                    ${e[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems
+}
+
